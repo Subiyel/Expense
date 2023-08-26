25b774ac6b6c28a45373ebde6f8686da6ebae17e
Subial <subialijaz@gmail.com>
2023-08-26 19:51:57 +0500

    Initial commit

---
+++ node/server.js
@@ -0,0 +1,68 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const passport = require("passport");
+
+// We load the Models
+require("./users//models/User");
+require("./posts//models/Post");
+
+const authRouter = require("./auth/auth.routes");
+const usersRouter = require("./users/users.routes");
+const postsRouter = require("./posts/posts.routes");
+require("dotenv").config();
+const app = express();
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  if (req.method === "OPTIONS") {
+    return res.send(200);
+  } else {
+    if (process.env.NODE_ENV != "test") {
+      console.log(req.originalUrl);
+    }
+    return next();
+  }
+});
+// Bodyparser middleware
+app.use(
+  bodyParser.urlencoded({
+    extended: false
+  })
+);
+app.use(bodyParser.json());
+
+// DB Config
+const db = process.env.MONGO_URI;
+if (process.env.NODE_ENV != "test") {
+  // Connect to MongoDB
+  mongoose
+    .connect(db, { useNewUrlParser: true })
+    .then(() => console.log("MongoDB successfully connected"))
+    .catch(err => console.log(err));
+}
+
+// Passport middleware
+app.use(passport.initialize());
+
+// Passport config
+require("./config/passport")(passport);
+
+// Routes
+app.use("/api/users", usersRouter);
+app.use("/api/auth", authRouter);
+app.use("/api/posts", postsRouter);
+app.get('/', (req, res) => {
+  res.send('Subiyel Server Live');
+})
+app.get("/favicon.ico", (req, res) => {
+  res.end();
+  console.log("favicon requested");
+  return;
+});
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => {
+  if (process.env.NODE_ENV != "test") {
+    console.log(`Server up and running on port ${port} !`);
+  }
+});
